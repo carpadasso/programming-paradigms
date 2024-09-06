8392b7e2b3308a0c20232de72c571089b40e397e
Leonardo Carpwiski <lionheartcarpwiski@gmail.com>
2024-09-06 12:29:13 -0300

    Update StudentsGrades.java

--- object-oriented/lecture-01/StudentsGrades.java
+++ object-oriented/lecture-01/StudentsGrades.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 public class StudentsGrades {
    public static void main(String args[]) throws IOException
    {
-      int i;
-      double p1, p2, p3, p4, maior;
+      int i, j;
+      double nota, maior, maiorAluno;
       Scanner teclado = new Scanner(System.in);
 
       maior = 0;
       for (i = 0; i < 30; i++){
+         maiorAluno = 0;
          System.out.print("Digite as notas: ");
-         p1 = teclado.nextDouble();
-         p2 = teclado.nextDouble();
-         p3 = teclado.nextDouble();
-         p4 = teclado.nextDouble();
+         for (j = 0; j < 4; j++){
+            nota = teclado.nextDouble();
+            if (nota > maiorAluno) maiorAluno = nota;
+            if (nota > maior) maior = nota;
+         }
 
-         System.out.println("Maior nota do " + (i+1) + " aluno: " + Math.max(Math.max(Math.max(p1, p2), p3), p4));
-         maior = Math.max(Math.max(Math.max(Math.max(p1, p2), p3), p4), maior);
+         System.out.println("Maior nota do " + (i+1) + " aluno: " + maiorAluno);
       }
 
       System.out.println("Maior nota da turma: " + maior);
